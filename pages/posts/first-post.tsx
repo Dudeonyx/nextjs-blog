960123fef3e632a22f8fdfc8dd9ced4c36a5a059
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2021-12-28 22:59:15 +0100

    added title to first-post

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,15 +1,21 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
-    <div>
-      <h1 className="text-3xl font-semibold">First Post</h1>;
-      <p>
-        Go back to{' '}
-        <Link href="/">
-          <a className="text-blue-600 hover:underline">homepage</a>
-        </Link>
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div>
+        <h1 className="text-3xl font-semibold">First Post</h1>
+        <p>
+          Go back to{' '}
+          <Link href="/">
+            <a className="text-blue-600 hover:underline">homepage</a>
+          </Link>
+        </p>
+      </div>
+    </>
   );
 }
